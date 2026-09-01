d394e65a708c915f636d553fbedc68c7fed7a663
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-01 11:05:45 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,19 @@
+# We are using and making funcation because sometimes  i need same things with very big , so insted of type same things multiple times ,
+# we make function of that thing , when we need we just call it function and perform a code 
+# so it make code very less line and less time .
+
+# for example we want to just calculate mean of fuction of just AM , i am taking example of Arithmetic mean of some numbers
+
+# let
+
+a = 2
+b = 3
+c = 8
+mean1 = (a+b+c)/3
+print(mean1)
+
+d = 7 
+e = 20
+f = 23
+mean2 = (d+e+f)/3
+print(mean2)
